87097bf31935ef6f892bd93707ec059f78d04a64
wesleyfelix <48739918+wesleyfelix@users.noreply.github.com>
2019-11-10 15:11:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res)=>{
     res.status(200).send({message: 'Olá mundo, estou funcionando!'});
 })
 
+app.get('/ola', (req, res)=>{
+    res.status(200).send({message: 'Oi, eu sou a rota Olá!'});
+})
+
 app.listen(3001, ()=> {
     console.log('Api rodando na porta 3001');
 })
